48faa4b9fc66b954e97da370522d25a416bb427c
amrutapawar1 <amrutapawar800@gmail.com>
2025-12-05 12:31:14 +0530

    updated

--- contact.php
+++ contact.php
@@ -1,55 +1,59 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $project = $_POST['project'] ?? '';
+    $fullname = $_POST['name'] ?? '';
+    $phone = $_POST['modal_my_mobile2'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $country_code = $_POST['country_code'] ?? '';
+    $country_name = $_POST['country_name'] ?? '';
+    $message= $_POST['message'] ?? '';
 
-$project = $_POST['project'];
-$name = $_POST['name'];
-$email = $_POST['email'];
-$contact = $_POST['modal_my_mobile2'];
-$country_code = $_POST['country_code'];
-$country_name = $_POST['country_name'];
-$message= $_POST['message'];
+    $to = "avadhut@bricspace.com";
+    $subject = "New Lead Notification - Website";
 
-$to = "avadhut@bricspace.com";
-$subject = "New Lead from Website !";
-
-$message = "
-<html>
-<head>
-<title>HTML email</title>
-</head>
-<body>
-<p>This email contains HTML Tags!</p>
-<table>
-<tr>ProjectName&#58; $project</tr>
-<tr>Name&#58; $name</tr>
-<tr>Email&#58; $email</tr>
-<tr>Phone&#58; $contact</tr>
-<tr>country_code&#58; $country_code</tr>
-<tr>country_name&#58; $country_name</tr>
-<tr>message&#58; $message</tr>
-</table>
-</body>
-</html>
-";
+    $message = "
+    <html>
+    <head>
+    
+    <title>New Lead</title></head>
+    <body>
+    <h3>Lead Details</h3>
+    <table border='1' cellspacing='0' cellpadding='6'>
+        <tr><td><strong>Project</strong></td><td>$project</td></tr>
+        <tr><td><strong>Name</strong></td><td>$fullname</td></tr>
+        <tr><td><strong>Phone</strong></td><td>$country_code$phone</td></tr>
+        <tr><td><strong>Email</strong></td><td>$email</td></tr>
+        <tr><td><strong>Country Name</strong></td><td>$country_name</td></tr>
+        <tr><td><strong>Message</strong></td><td>$message</td></tr>
+    </table>
+    </body>
+    </html>
+    ";
 
 // Always set content-type when sending HTML email
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 // More headers
-$headers .= 'From: <avadhut@bricspace.com>' . "\r\n";
-$headers .= 'Cc: avadhut@bricspace.com\r\n';
-
+$headers .= 'From: <no_reply@GodrejPtoperties.com>' . "\r\n";
+$headers .= 'Cc: botmediadigitalmarketing@gmail.com' . "\r\n";
 
 if(mail($to,$subject,$message,$headers)) //Send an Email. Return true on success or false on error
+
+
 {
-echo "<script>window.location.href='index.html';</script>";
+echo 
+
+"<script>window.location.href='thankyou.html';</script>";
 }
 else
 {
-echo "<script> 
+echo "<script>
 alert('Plz Try Agian');
-window.location.href='index.html';
+window.location.href='index.html'
+
+;
 </script>";
 }
-
+}
 ?>
